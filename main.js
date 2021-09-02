8d18b17642ebdb20376f4125c65ab28a656a35b7
Charlie <ruggedcafe@gmail.com>
2021-09-02 21:31:35 +0900

    기본 nodejs 구문 expressjs 형태로 구현

--- main.js
+++ main.js
@@ -162,8 +162,10 @@ const app = express()
 const fs = require('fs')
 const template = require('./lib/template.js')
 const port = 3000
+
 var sanitizeHtml = require('sanitize-html');
 var path = require('path');
+var qs = require('querystring');
 
 app.get('/', (request, response) => {
  
@@ -195,17 +197,110 @@ app.get('/page/:pageId', (request, response) => {
       var html = template.html(title, list
         , `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`
         , `<a href="/create">create</a>
-           <a href="/update?id=${sanitizedTitle}">update</a>
-           <form action="delete_process" method="post" onsubmit="return confirm('${sanitizedTitle}파일을 삭제하시겠습니까?')">
+           <a href="/update/${sanitizedTitle}">update</a>
+           <form action="/delete_process" method="post" onsubmit="return confirm('${sanitizedTitle}파일을 삭제하시겠습니까?')">
               <input type="hidden" name="id" value="${sanitizedTitle}">
               <input type="submit" value="delete">
            </form>
            `);
-      response.writeHead(200);
-      response.end(html);
+      response.send(html);
       });
   });
      
+});
+
+app.get('/create', (request, response) => {
+        fs.readdir('./data', function(err, filelist){
+        var title = 'WEB - CREATE';
+        var list = template.list(filelist);
+        var html = template.html(title, list,
+            `<form class="" action="/create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+
+              </p><textarea name="description" rows="8" cols="80" placeholder="description"></textarea>
+              <p>
+                <input type="submit" name="" value="create">
+              </p>
+            </form>
+            `,``);
+          response.send(html);
+      });
+});
+
+app.post('/create_process', (request, response) => {
+        var body = '';
+        request.on('data', function(data){
+          body += data;
+        });
+  
+        request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/page/${title}`});
+            response.end();
+          });
+        });
+});
+
+app.get('/update/:pageId', (request, response) => {
+    fs.readdir('./data', function(err, filelist){
+        var filteredId = path.parse(request.params.pageId).base;
+      fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = filteredId;
+      var list = template.list(filelist);
+      var html = template.html(title, list,
+        `<form class="" action="/update_process" method="post">
+          <input type="text" name="id" value="${title}" hidden="hidden">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+
+          </p><textarea name="description" rows="8" cols="80" placeholder="description">${description}</textarea>
+          <p>
+            <input type="submit" name="" value="update">
+          </p>
+        </form>
+        `,``);
+        response.send(html);
+          });
+        });
+});
+
+app.post('/update_process', (request, response) => {
+  var body = '';
+        request.on('data', function(data){
+          body += data;
+        });
+  
+        request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          console.log("post = " + JSON.stringify(post));
+          fs.rename(`data/${id}`, `data/${title}`, function(){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.redirect(`/page/${title}`);
+            });
+          });
+        });
+});
+
+app.post('/delete_process', (request, response) => {
+      var body = '';
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var filteredId = path.parse(id).base;
+        fs.unlink(`data/${filteredId}`, function(err){
+          response.redirect('/');
+        });
+      });
 });
 
 app.listen(port, () => {
